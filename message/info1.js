1b5395b79bd1537a8b96cba2596b6e33639160e1
SPXBot <irvanaditeca98@gmail.com>
2021-12-28 18:40:55 +0700

    first commit

---
+++ message/info1.js
@@ -0,0 +1,29 @@
+exports.bot = (gender,fake,nomerOwner)=> {
+return`
+*╭─▸*「 𝗜𝗡𝗙𝗢 𝗕𝗢𝗧𝗭 」
+*│*
+*│▸* Nama : ${fake}
+*│▸* Gender : ${gender}
+*│▸* Hoby : -
+*│▸* Owner : ${nomerOwner}
+*│▸* Tgl pembuatan : 25-07-2021
+*│▸* Tgl release : 29-09-2021
+*│▸* Image : Elaina
+*│▸* Voice : Nina kawai
+*│*
+*│▸* SCRIPT BY
+*│*   Decode Denpa
+*│*   Dttaz
+*│*   AlyaaChann
+*│*
+*│▸* FEATURE  BY  
+*│*   Decode Denpa
+*│*   X-Dev Team
+*│*   AlyaaChann
+*│*   Zeeone Ofc
+*│*   X-Dev Team
+*│*
+*╰────────────[*`
+}
+
+
